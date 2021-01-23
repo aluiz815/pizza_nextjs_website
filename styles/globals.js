6929542790f31de2067929fbe316aa58889667d6
Andre Luiz <aluiz815@gmail.com>
2021-01-23 01:13:55 -0300

    styled Components Added

---
+++ styles/globals.js
@@ -0,0 +1,23 @@
+import {createGlobalStyle} from 'styled-components';
+
+
+export default createGlobalStyle`
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html,body,#__next {
+  width: 100vw;
+  height: 100vh;
+}
+
+a{
+  text-decoration: none;
+  cursor: pointer;
+}
+` 
+
+

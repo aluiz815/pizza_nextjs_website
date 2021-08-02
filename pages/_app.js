83e1aed8821087d75d4b490d6aba0a9f7670e480
Andre Luiz <aluiz815@gmail.com>
2021-08-02 18:39:40 -0300

    Test Webpush

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,15 @@ function MyApp({ Component, pageProps }) {
         <title>Italian Pizza</title>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;900&display=swap" rel="stylesheet"/>
+        <script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""/>
+        <script>
+          window.OneSignal = window.OneSignal || [];
+          OneSignal.push(function() {
+            OneSignal.init({
+              appId: "8b0eed18-f9b2-4727-9bba-0e5a6e14ef78",
+            })
+          })
+        </script>
       </Head>
       <GlobalStyles/>
       <Component {...pageProps} />

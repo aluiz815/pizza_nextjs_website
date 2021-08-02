659bd8c7833987b8cf603137e1fdcfb4d465acae
Andre Luiz <aluiz815@gmail.com>
2021-08-02 18:54:11 -0300

    Test Webpush 2

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
+import { useEffect } from 'react';
 import Feature from '../src/components/Feature';
 import Footer from '../src/components/Footer';
 import Hero from '../src/components/Hero';
 import Products from '../src/components/Products';
 export default function Home() {
+  useEffect(()=>{
+  window.OneSignal = window.OneSignal || [];
+  OneSignal.push(function () {
+      OneSignal.init({
+        appId: "ae1ed497-4cd1-43fb-bfbd-d7b633bbe410",
+        safari_web_id: "web.onesignal.auto.4c35e1cb-920a-4e68-be7d-eea8363a1cf4",
+          notifyButton: {
+              enable: true,
+          },
+
+          allowLocalhostAsSecureOrigin: true,
+      });
+  });
+  return () => {
+      window.OneSignal = undefined;
+  };
+  },[])
   return (
     <>
       <Hero/>

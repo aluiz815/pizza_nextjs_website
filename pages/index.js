bc2057e62208cba59a6c38e97939600409baee42
Andre Luiz <aluiz815@gmail.com>
2021-08-13 13:10:29 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,8 @@ export default function Home() {
 
   useEffect(()=>{
     async function startOneSignal() {
-      window.OneSignal = await window.OneSignal || [];
-  OneSignal.push(function () {
+    window.OneSignal = await window.OneSignal || [];
+    OneSignal.push(function () {
     OneSignal.init({
       appId: "ae1ed497-4cd1-43fb-bfbd-d7b633bbe410",
       safari_web_id: "web.onesignal.auto.56640081-e0ac-44c5-9d26-db4b2624f604",
@@ -22,22 +22,7 @@ export default function Home() {
       OneSignal.on('subscriptionChange', function (isSubscribed) {
         if(isSubscribed) {
           OneSignal.getUserId(async function(userId) {
-            if(logged) {
-              await axios.put(`https://webpush-andre.herokuapp.com/update/user/${userId}`,{
-                DEKEY:"5F880AFC-20EB-4C0E-91FA-F91028D6D5D5",
-                values:{
-                  email:'teste@teste.com'
-                }
-              })
-            } 
-
-              await axios.post('https://webpush-andre.herokuapp.com/store/user',{
-                DEKEY:"5F880AFC-20EB-4C0E-91FA-F91028D6D5D5",
-                items:[{
-                  player_id:userId
-                }]
-              })
-            
+            console.log(userId)
            });
         }
       });

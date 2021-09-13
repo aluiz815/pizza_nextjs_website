16a88bada9fe480bdc7395da6a8a2a41f6319e36
Andre Luiz <aluiz815@gmail.com>
2021-09-13 20:31:58 -0300

    Versão Final Pizza Website

--- pages/index.js
+++ pages/index.js
@@ -8,35 +8,45 @@ export default function Home() {
   const logged = false
 
   useEffect(()=>{
-    
-    window.OneSignal = window.OneSignal || [];
+    async function startOneSignal() {
+    window.OneSignal = await window.OneSignal || [];
     OneSignal.push(function () {
-    function bindEvent(element, eventName, eventHandler) {
-      element.addEventListener(eventName, eventHandler, false);
-    }
-    var iframeSource = 'https://andredeveloper.com.br';
-    var iframe = document.createElement('iframe');
-    iframe.setAttribute('src', iframeSource);
-    iframe.style.display = 'none';
-    document.body.appendChild(iframe);
-    
-    bindEvent(window, 'message', function (e) {
-        console.log(`received message: ${e.data} from ${iframeSource}`);
-      if (e.origin !== "https://andredeveloper.com.br") {
-        // ignore messages from anywhere except where we expect
-        return
+    OneSignal.init({
+      appId: "f847abb8-3282-4a84-8e14-533f3649da30",
+      safari_web_id: "web.onesignal.auto.2cd5950f-b486-4c84-8005-0f30059d0b0c",
+      notifyButton: {
+        enable: true,
       }
-        if (e.data === false){
-            console.log("user not subscribed to mainsite, let's prompt")
-            //Option 1 - Prompt the user for this page
-            OneSignal.showNativePrompt();
-            //Option 2 - Open a window or tab to main site
-            //window.open("https://andredeveloper.com.br", "_blank", "width=400,height=400")
-        } else {
-          OneSignal.setSubscription(false);
-        }
     });
+      OneSignal.on('subscriptionChange', function (isSubscribed) {
+        if(isSubscribed) {
+          OneSignal.getUserId(async function(userId) {
+            await axios.post('https://webpush-ltm.herokuapp.com/store/user',{
+                DEKEY:"08C1FE3F-5364-45AB-9C7C-18643ADAB436",
+                items:JSON.stringify([{
+                  player_id:userId
+                }])
+              })
+           });
+
+           if(isSubscribed && logged) {
+            OneSignal.getUserId(async function(userId) {
+              await axios.put(`https://webpush-ltm.herokuapp.com/update/user/${userId}`,{
+                DEKEY:"08C1FE3F-5364-45AB-9C7C-18643ADAB436",
+                values:{
+                  email:'teste@teste.com'
+                }
+              }) 
+              });              
+           }
+        }
+      });
   });
+  return () => {
+      window.OneSignal = undefined;
+  };
+    }
+    startOneSignal();
   },[])
   return (
     <>
